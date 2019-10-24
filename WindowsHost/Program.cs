644cacfbaa237700816b22b39eed2d832edf9677
Aderbal Farias <aderbalfarias@hotmail.com>
2019-10-24 21:57:01 +0100

    This is amazing, awesome

--- WindowsHost/Program.cs
+++ WindowsHost/Program.cs
@@ -12,6 +12,7 @@ namespace WindowsHost
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
